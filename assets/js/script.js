d228752da5667dabf37001b80c2e821551c4c0df
JonWong2020 <jonathannwong50@aol.com>
2022-08-22 02:49:32 -0700

    edited js

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+// Query Selectors
+
+var startQuizButton = document.querySelector("#start-quiz-button");
+var quizIntro = document.querySelector(".quiz-intro");
+var questionSection = document.querySelector(".question-section");
+var topBar = document.querySelector(".top-bar");
+var timerDisplay = topBar.querySelector("p");
+var answerSection = document.querySelector("ul");
+var quizFinish = document.querySelector(".quiz-finish-section");
+var main = document.querySelector("main");
+var scoreEntry = document.querySelector("#score-entry");
+var scoreSubmitButton = scoreEntry.querySelector("button");
+var initialsEntry = scoreEntry.querySelector("input");
+var highscoresScreen = document.querySelector(".highscores-screen");
+var viewHighScoresButton = topBar.querySelector("button");
+
+// Display questions
+
