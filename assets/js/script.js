251073155601a2a4a8cf12d7fd4f54af1d34f37b
JonWong2020 <jonathannwong50@aol.com>
2022-08-22 16:32:02 -0700

    added timer function

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,4 +15,61 @@ var highscoresScreen = document.querySelector(".highscores-screen");
 var viewHighScoresButton = topBar.querySelector("button");
 
 // Display questions
+var quizQuestions = [
+    {
+        question: "Which of the following is an example of an array?",
+        answers: {
+            a: "var array = {}",
+            b: "var array = ()",
+            c: "var array = <>",
+            d: "var array = []",
+        },
+        correctAnswer: "d",
+    },
+    {
+        question: "Which DOM method is used to create a new HTML element",
+        answers: {
+            a: "document.createElement()",
+            b: "document.newElement()",
+            c: "document.element()",
+            d: "document.spawnElement()",
+        },
+        correctAnswer: "a",
+    },
+    {
+        question: "Which operator represents OR statements?",
+        answers: {
+            a: "&&",
+            b: "||",
+            c: "==",
+            d: "()",
+        },
+        correctAnswer: "b",
+    }
+]
 
+// Timer function
+function startTimer() {
+    timerCount = 60;
+
+    var quizTimer = setInterval(function() {
+        timerCount--
+        timerDisplay.textContent = timerCount
+
+        if (timerCount === 00) {
+            clearInterval(quizTimer);
+        }
+    }, 1000);
+}
+
+// Display quiz
+function displayQuiz()
+
+// Quiz start
+function startQuiz() {
+    startTimer();
+    return;
+}
+
+// Event listeners
+startQuizButton.addEventListener("click", startQuiz);
