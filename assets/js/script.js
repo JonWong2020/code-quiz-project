7a50d3ee75c42101ea31d6c0206c3006434a8e3c
JonWong2020 <jonathannwong50@aol.com>
2022-08-22 23:20:35 -0700

    edited css and js

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,10 +11,12 @@ var main = document.querySelector("main");
 var scoreEntry = document.querySelector("#score-entry");
 var scoreSubmitButton = scoreEntry.querySelector("button");
 var initialsEntry = scoreEntry.querySelector("input");
-var highscoresScreen = document.querySelector(".highscores-screen");
+var viewHighScoresScreen = document.querySelector(".highscores-screen");
 var viewHighScoresButton = topBar.querySelector("button");
+var returnButton = viewHighScoresScreen.querySelector("#returnbutton");
 
 // Other variables
+
 var quizWin = false
 
 // Display questions
@@ -22,61 +24,80 @@ var quizQuestions = [
     {
         question: "Which of the following is an example of an array?",
         answers: {
-            a: "var array = {}",
-            b: "var array = ()",
-            c: "var array = <>",
-            d: "var array = []",
+            choiceA: "var array = {}",
+            choiceB: "var array = ()",
+            choiceC: "var array = <>",
+            choiceD: "var array = []",
         },
         correctAnswer: "d",
     },
     {
         question: "Which DOM method is used to create a new HTML element",
         answers: {
-            a: "document.createElement()",
-            b: "document.newElement()",
-            c: "document.element()",
-            d: "document.spawnElement()",
+            choiceA: "document.createElement()",
+            choiceB: "document.newElement()",
+            choiceC: "document.element()",
+            choiceD: "document.spawnElement()",
         },
         correctAnswer: "a",
     },
     {
         question: "Which operator represents OR statements?",
         answers: {
-            a: "&&",
-            b: "||",
-            c: "==",
-            d: "()",
+            choiceA: "&&",
+            choiceB: "||",
+            choiceC: "==",
+            choiceD: "()",
         },
         correctAnswer: "b",
-    }
+    },
+    {
+        question: "What does CSS stand for?",
+        answers: {
+            choiceA: "Common Style Sheets",
+            choiceB: "Colorful Style Sheets",
+            choiceC: "Cascading Style Sheets",
+            choiceD: "Computer Style Sheets",
+        },
+        correctAnswer: "c",
+    },
 ]
 
 // Quiz start
 function startQuiz() {
     quizWin = false;
     timerCount = 60;
-    startQuizButton.disabled = true
+    // viewHighScoresButton.disabled = true
     startTimer();
     displayQuiz();
     return;
 }
 
+// Display intro
+function displayIntro() {
+    quizIntro.setAttribute("style", "display: block");
+    questionSection.setAttribute("style", "display: none");
+    quizFinish.setAttribute("style", "display: none");
+    viewHighScoresScreen.setAttribute("style", "display: none");
+    // viewHighScoresButton.enabled = true; 
+}
+
 // Display quiz
 function displayQuiz() {
     quizIntro.setAttribute("style", "display: none");
-    questionSection.setAttribute("style", "display:block");
+    questionSection.setAttribute("style", "display: block");
+    
 }
-
 
 
 // Quiz Complete function
 function quizComplete() {
     quizIntro.setAttribute("style", "display: none");
-    questionSection.setAttribute("style", "display:none");
-    quizFinish.setAttribute("style", "display:block");
-    if (initialsEntry = "" && scoreSubmitButton.addEventListener("click")) {
-        alert("Please enter your initials");
-    }
+    questionSection.setAttribute("style", "display: none");
+    quizFinish.setAttribute("style", "display: block");
+    // if (initialsEntry = "" && scoreSubmitButton.addEventListener("click")) {
+    //     alert("Please enter your initials");
+    // }
 }
 
 // Timer function
@@ -89,14 +110,29 @@ function startTimer() {
             if (quizWin && timerCount > 0) {
                 clearInterval(quizTimer);
                 quizComplete();
+                startQuizButton.enabled = true;
+                viewHighScoresButton.enabled = true;
             }
         }
         if (timerCount === 0) {
             clearInterval(quizTimer);
             quizComplete();
+            startQuizButton.enabled = true;
+            viewHighScoresButton.enabled = true;
         }
     }, 1000);
 }
 
+// View highscores section
+function displayHighScores() {
+    quizIntro.setAttribute("style", "display: none");
+    questionSection.setAttribute("style", "display: none");
+    quizFinish.setAttribute("style", "display: none");
+    viewHighScoresScreen.setAttribute("style", "display: block");
+}
+
 // Event listeners
 startQuizButton.addEventListener("click", startQuiz);
+viewHighScoresButton.addEventListener("click", displayHighScores);
+returnButton.addEventListener("click", displayIntro);
+scoreSubmitButton.addEventListener("click", displayIntro);
