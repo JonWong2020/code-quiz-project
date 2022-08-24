326328d28ec48f8bade9277341b6dec1bb9741f8
JonWong2020 <jonathannwong50@aol.com>
2022-08-24 14:04:08 -0700

    added display ques/answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,63 +10,66 @@ var quizFinish = document.querySelector(".quiz-finish-section");
 var main = document.querySelector("main");
 var scoreEntry = document.querySelector("#score-entry");
 var scoreSubmitButton = scoreEntry.querySelector("button");
-var initialsEntry = scoreEntry.querySelector("input");
+var initialsEntry = scoreEntry.querySelector("input").value;
 var viewHighScoresScreen = document.querySelector(".highscores-screen");
 var viewHighScoresButton = topBar.querySelector("button");
-var returnButton = viewHighScoresScreen.querySelector("#returnbutton");
+var returnButton = viewHighScoresScreen.querySelector("#return-button");
+var highScoresList = viewHighScoresScreen.querySelector("#hs-list");
+var questionHead = questionSection.querySelector("#question-head");
 
 // Other variables
 
-var quizWin = false
+var quizWin = false;
+var questionIndex = 0;
 
 // Display questions
 var quizQuestions = [
     {
         question: "Which of the following is an example of an array?",
-        answers: {
-            choiceA: "var array = {}",
-            choiceB: "var array = ()",
-            choiceC: "var array = <>",
-            choiceD: "var array = []",
-        },
-        correctAnswer: "d",
+        answers: [
+             "var array = {}",
+             "var array = ()",
+             "var array = <>",
+            "var array = []",
+        ],
+        correctAnswer: "var array = []",
     },
     {
         question: "Which DOM method is used to create a new HTML element",
-        answers: {
-            choiceA: "document.createElement()",
-            choiceB: "document.newElement()",
-            choiceC: "document.element()",
-            choiceD: "document.spawnElement()",
-        },
-        correctAnswer: "a",
+        answers: [
+             "document.createElement()",
+             "document.newElement()",
+             "document.element()",
+            "document.spawnElement()",
+        ],
+        correctAnswer: "document.createElement()",
     },
     {
         question: "Which operator represents OR statements?",
-        answers: {
-            choiceA: "&&",
-            choiceB: "||",
-            choiceC: "==",
-            choiceD: "()",
-        },
-        correctAnswer: "b",
+        answers: [
+            "&&",
+            "||",
+            "==",
+            "()",
+        ],
+        correctAnswer: "||",
     },
     {
         question: "What does CSS stand for?",
-        answers: {
-            choiceA: "Common Style Sheets",
-            choiceB: "Colorful Style Sheets",
-            choiceC: "Cascading Style Sheets",
-            choiceD: "Computer Style Sheets",
-        },
-        correctAnswer: "c",
+        answers: [
+            "Common Style Sheets",
+            "Colorful Style Sheets",
+            "Cascading Style Sheets",
+            "Computer Style Sheets",
+        ],
+        correctAnswer: "Cascading Style Sheets",
     },
-]
+];
 
 // Quiz start
 function startQuiz() {
     quizWin = false;
-    timerCount = 60;
+    timerCount = 15;
     // viewHighScoresButton.disabled = true
     startTimer();
     displayQuiz();
@@ -86,18 +89,66 @@ function displayIntro() {
 function displayQuiz() {
     quizIntro.setAttribute("style", "display: none");
     questionSection.setAttribute("style", "display: block");
-    
+
+    // For loops to loop through all info in array
+    for (var i = 0; i < quizQuestions.length; i++) {
+        // Appends question title only
+        var userQuestion = quizQuestions[questionIndex].question;
+        var userChoices = quizQuestions[questionIndex].answers;
+        questionHead.textContent = userQuestion;
+    }
+    // New for each for question choices
+    userChoices.forEach(function (newItem) {
+        
+        var listItem = document.createElement("li");
+        
+        listItem.textContent = newItem;
+        questionSection.appendChild(questionHead);
+        questionHead.appendChild(listItem);
+        listItem.addEventListener("click", (compare));
+    })
 }
 
+// Check answers
+function compare(event) {
+    var element = event.target;
+
+    if (element.matches("li")) {
+
+        var createDiv = document.createElement("div");
+        createDiv.setAttribute("id", "createDiv");
+        // Correct condition 
+        if (element.textContent == question[questionIndex].correctAnswer) {
+            score++;
+            createDiv.textContent = "Correct! The answer is:  " + question[questionIndex].correctAnswer;
+            // Correct condition 
+        } else {
+            // Will deduct -5 seconds off secondsLeft for wrong answers
+            secondsLeft = secondsLeft - penalty;
+            createDiv.textContent = "Wrong! The correct answer is:  " + question[questionIndex].correctAnswer;
+        }
+
+    }
+    
+    questionIndex++;
+
+    if (questionIndex >= question.length) {
+        
+        quizComplete();
+        createDiv.textContent = "End of quiz!" + " " + "You got  " + score + "/" + questions.length + " Correct!";
+    } else {
+        render(questionIndex);
+    }
+    questionSection.appendChild(createDiv);
+
+}
 
 // Quiz Complete function
 function quizComplete() {
     quizIntro.setAttribute("style", "display: none");
     questionSection.setAttribute("style", "display: none");
     quizFinish.setAttribute("style", "display: block");
-    // if (initialsEntry = "" && scoreSubmitButton.addEventListener("click")) {
-    //     alert("Please enter your initials");
-    // }
+  
 }
 
 // Timer function
@@ -135,4 +186,4 @@ function displayHighScores() {
 startQuizButton.addEventListener("click", startQuiz);
 viewHighScoresButton.addEventListener("click", displayHighScores);
 returnButton.addEventListener("click", displayIntro);
-scoreSubmitButton.addEventListener("click", displayIntro);
+// scoreSubmitButton.addEventListener("click", addHighScore);
